80b7e1be7e91ae6e90f7c2005016e213d6f91205
Ivan Yaremchuk <lntu.yaremcha@gmail.com>
2022-09-16 15:30:04 +0300

    Refactoring

--- app/src/main/java/com/example/test/ui/base/OnChange.kt
+++
@@ -1,5 +0,0 @@
-package com.example.test.ui.base
-
-class OnChange<T>(
-    val value: T
-)
--- app/src/main/java/com/example/test/ui/screens/RootViewModel.kt
+++ app/src/main/java/com/example/test/ui/screens/RootViewModel.kt
@@ -1,14 +1,10 @@
 package com.example.test.ui.screens
 
-import androidx.annotation.StringRes
 import androidx.lifecycle.*
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.test.domain.Gif
 import com.example.test.domain.GifsRepository
-import com.example.test.ui.base.MutableLiveEvent
-import com.example.test.ui.base.publishEvent
-import com.example.test.ui.base.share
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
@@ -22,18 +18,6 @@ class RootViewModel @Inject constructor(
 ) : ViewModel() {
 
     val gifsBySearchFlow: Flow<PagingData<Gif>>
-
-    private val _scrollEvents = MutableLiveEvent<Unit>()
-    val scrollEvents = _scrollEvents.share()
-
-    private val _errorEvents = MutableLiveEvent<Int>()
-    val errorEvents = _errorEvents.share()
-
-    private val _toastEvent = MutableLiveEvent<Int>()
-    val toastEvent = _toastEvent.share()
-
-    private var _invalidateEvents = MutableLiveEvent<Unit>()
-    val invalidateEvents = _invalidateEvents.share()
 
     private val searchLiveData =
         savedStateHandle.getLiveData(KEY_YEAR, "")
@@ -50,19 +34,6 @@ class RootViewModel @Inject constructor(
                 gifsRepository.getGifs(it)
             }
             .cachedIn(viewModelScope)
-    }
-
-    private fun scrollListToTop() {
-        _scrollEvents.publishEvent(Unit)
-    }
-
-
-    private fun invalidateList() {
-        _invalidateEvents.publishEvent(Unit)
-    }
-
-    private fun showError(@StringRes errorMessage: Int) {
-        _errorEvents.publishEvent(errorMessage)
     }
 
     private companion object {
